c05a5b06ee31bd46ac652c17bb34453ed91b8bd4
yellow <ryanbuenaventura321@gmail.com>
2020-06-11 23:48:29 +0800

    book checkout sequence

--- app/Book.php
+++ app/Book.php
@@ -3,6 +3,7 @@
 namespace App;
 use App\Book;
 use App\Author;
+use App\Reservation;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,29 @@ class Book extends Model
 {
     protected $guarded = [];
     
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id'=>$user->id,
+            'checked_out_at'=>now()
+        ]);
+    }
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()->where('user_id',$user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if(is_null($reservation))
+        {
+            throw new \Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at'=>now()
+        ]);
+    }
     public function path(){
         return '/books/'.$this->id;
     }
@@ -19,4 +43,8 @@ class Book extends Model
             'name'=>$author
         ]))->id;
     }
+    public function reservations(){
+        return $this->hasMany(Reservation::class);
+    }
+    
 }
